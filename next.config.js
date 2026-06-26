3f1d9acdb59e03a98cf6112efdaa79b4d6ec39f8
iKasperenok <81311082+iKasperenok@users.noreply.github.com>
2026-06-26 12:44:18 +0300

    1

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+//  reactStrictMode: true,
+  output: 'export',
+    images: {
+      unoptimized: true, // обязательно для shared-хостинга
+    },
 }
 
 module.exports = nextConfig
